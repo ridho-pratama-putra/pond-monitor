6385db2d21ac15cecd270a3dcd75a34c4a29ffa1
ridho <ridho.putra@btpn.com>
2022-06-09 17:35:21 +0700

    add kafka config

---
+++ src/main/java/com/lotus/pond/monitor/configuration/KafkaConfiguration.java
@@ -0,0 +1,33 @@
+package com.lotus.pond.monitor.configuration;
+
+import com.fasterxml.jackson.databind.ser.std.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.Map;
+
+import static org.apache.kafka.clients.CommonClientConfigs.RETRIES_CONFIG;
+import static org.apache.kafka.clients.producer.ProducerConfig.*;
+
+@Configuration
+public class KafkaConfiguration {
+
+    @Bean
+    public ProducerFactory<String,String> producerFactory(){
+        return new DefaultKafkaProducerFactory<>(
+                Map.of(BOOTSTRAP_SERVERS_CONFIG,"localhost:9092",
+                        RETRIES_CONFIG,0,
+                        BUFFER_MEMORY_CONFIG,33554432,
+                        KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class,
+                        VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class
+                ));
+    }
+
+    @Bean
+    public KafkaTemplate<String,String> kafkaTemplate(){
+        return new KafkaTemplate<>(producerFactory());
+    }
+}
