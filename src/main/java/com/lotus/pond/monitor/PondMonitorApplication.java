8f8be2b25f0f2a2dd297b164cc5f7c4363e9fb1a
ridho <ridho.putra@btpn.com>
2022-06-09 16:15:32 +0700

    init

---
+++ src/main/java/com/lotus/pond/monitor/PondMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.lotus.pond.monitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PondMonitorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PondMonitorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lotus/pond/monitor/PondMonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lotus.pond.monitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PondMonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
