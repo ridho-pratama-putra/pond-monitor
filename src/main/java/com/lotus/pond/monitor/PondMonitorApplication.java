c6abe202e96f185ef2140040093e7c1ccf5e1f46
ridho <ridho.putra@btpn.com>
2022-06-11 10:20:29 +0700

    example of add topic programmatically

--- src/main/java/com/lotus/pond/monitor/PondMonitorApplication.java
+++ src/main/java/com/lotus/pond/monitor/PondMonitorApplication.java
@@ -1,13 +1,23 @@
 package com.lotus.pond.monitor;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.TopicBuilder;
 
 @SpringBootApplication
 public class PondMonitorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PondMonitorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PondMonitorApplication.class, args);
+    }
 
+    @Bean
+    NewTopic programmaticallyCreateTopic() {
+        return TopicBuilder.name("programmatically.topic")
+                .partitions(4)
+                .replicas(3)
+                .build();
+    }
 }
